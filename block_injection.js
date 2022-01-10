948f2321f1a7ddb76b95af7232ec819d70655c93
nascvi <vaiiho.nascimbene@gmail.com>
2022-01-10 03:48:55 +0100

    latest 100 block numbers ie

--- block_injection.js
+++ block_injection.js
@@ -2,10 +2,18 @@ let Web3 = require('web3')
 let url = 'https://mainnet.infura.io/v3/cfc8a67431274099a151d7de778d7e37'
 let web3 = new Web3(url)
 
-web3.eth.getBlockNumber().then((result)=>{
-    console.log(result);
-})
+// web3.eth.getBlockNumber().then((result)=>{
+//     console.log(result);
+// })
 
-web3.eth.getBlock(13975129).then((block)=>{
-    console.log(block);
+// web3.eth.getBlock(13975129).then((block)=>{
+//     console.log(block);
+// })
+
+web3.eth.getBlockNumber().then((result)=>{
+    for(let i=0;i<100;i++){
+        web3.eth.getBlock(result-i).then((block)=>{
+            console.log(block.number);
+        })
+    };
 })
